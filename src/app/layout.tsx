e26e1fb6fc766d91e3228fae342542e85840e001
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-11-04 14:52:45 +0700

    #31: build ProviderTree component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,31 +2,22 @@
 import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import { Lexend } from 'next/font/google';
-import { Provider } from 'react-redux';
-import { Experimental_CssVarsProvider as CssVarsProvider, StyledEngineProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import { CacheProvider } from '@emotion/react';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ToastContainer } from 'react-toastify';
 import AppBar from '@/components/AppBar';
 import BackdropLoading from '@/components/Loading/Backdrop';
 import GlobalInfo from '@/components/GlobalInfo';
-import theme from '@/common/styles/theme';
-import createEmotionCache from '@/common/styles/createEmotionCache';
+import ProviderTree from '@/components/Provider';
 import { withAuth } from '@/hocs';
-import store from '@/redux/store';
 import 'react-toastify/dist/ReactToastify.css';
 import '@/common/styles/globals.css';
 
-const clientSideEmotionCache = createEmotionCache();
 const roboto = Lexend({
   weight: '400',
   subsets: ['latin'],
   display: 'swap',
 });
-const queryClient = new QueryClient();
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -38,42 +29,29 @@ function RootLayout({ children }: { children: React.ReactNode }) {
 
   return (
     <html lang="en" className={roboto.className}>
-      <Provider store={store}>
-        <StyledEngineProvider injectFirst>
-          <CacheProvider value={clientSideEmotionCache}>
-            <CssVarsProvider theme={theme}>
-              <CssBaseline />
-              <QueryClientProvider client={queryClient}>
-                <body>
-                  {!isLoaded ? (
-                    <BackdropLoading />
-                  ) : (
-                    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
-                      {pathname.startsWith('/auth') ? <></> : <AppBar />}
-                      {!isLoaded ? (
-                        <BackdropLoading />
-                      ) : (
-                        <Box sx={{ bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#24272b' : 'white') }}>
-                          <GlobalInfo>{children}</GlobalInfo>
-                        </Box>
-                      )}
-                    </Container>
-                  )}
-                  <ToastContainer
-                    position="top-right"
-                    autoClose={3000}
-                    closeOnClick
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="colored"
-                  />
-                </body>
-              </QueryClientProvider>
-            </CssVarsProvider>
-          </CacheProvider>
-        </StyledEngineProvider>
-      </Provider>
+      <ProviderTree>
+        <body>
+          {!isLoaded ? (
+            <BackdropLoading />
+          ) : (
+            <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+              {pathname.startsWith('/auth') ? <></> : <AppBar />}
+              <Box sx={{ bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#24272b' : 'white') }}>
+                <GlobalInfo>{children}</GlobalInfo>
+              </Box>
+            </Container>
+          )}
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            closeOnClick
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+        </body>
+      </ProviderTree>
     </html>
   );
 }
---
+++ src/components/Provider/index.tsx
@@ -0,0 +1,27 @@
+import { Provider } from 'react-redux';
+import { Experimental_CssVarsProvider as CssVarsProvider, StyledEngineProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import store from '@/redux/store';
+import theme from '@/common/styles/theme';
+import createEmotionCache from '@/common/styles/createEmotionCache';
+import { buildProviderTree } from './service';
+
+const clientSideEmotionCache = createEmotionCache();
+const queryClient = new QueryClient();
+
+function ProviderTree({ children }: { children: React.ReactNode }) {
+  const ProviderTree = buildProviderTree([
+    [Provider, { store }],
+    [StyledEngineProvider, { injectFirst: true }],
+    [CacheProvider, { value: clientSideEmotionCache }],
+    [CssVarsProvider, { theme }],
+    [CssBaseline],
+    [QueryClientProvider, { client: queryClient }],
+  ]);
+
+  return <ProviderTree>{children}</ProviderTree>;
+}
+
+export default ProviderTree;
