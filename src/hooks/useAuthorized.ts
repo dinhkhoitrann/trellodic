de58bfdfbb5f2ff118159a9559e1a9c339082009
Tran Dinh Khoi <trandinhkhoi102@gmail.com>
2023-12-14 17:47:40 +0700

    #42: authorize for workspace

--- src/hooks/useAuthorized.ts
+++ src/hooks/useAuthorized.ts
@@ -1,18 +1,28 @@
 import { useAppSelector } from '@/redux/store';
 import { selectBoardDetails } from '@/redux/slices/board';
 import { selectUserProfile } from '@/redux/slices/user';
+import { selectWorkspaceDetails } from '@/redux/slices/workspace';
 
 function checkBoardAdmin(adminId: string | undefined, userId: string | undefined) {
   if (!adminId || !userId) return false;
   return adminId === userId;
 }
 
+function checkWorkspaceAdmin(ownerId: string | undefined, userId: string | undefined) {
+  if (!ownerId || !userId) return false;
+  return ownerId === userId;
+}
+
 export default function useAuthorized() {
   const board = useAppSelector(selectBoardDetails);
+  const workspace = useAppSelector(selectWorkspaceDetails);
   const user = useAppSelector(selectUserProfile);
+
   const isBoardAdmin = checkBoardAdmin(board.admin as string, user?._id);
+  const isWorkspaceAdmin = checkWorkspaceAdmin(workspace.ownerUserId, user?._id);
 
   return {
     isBoardAdmin,
+    isWorkspaceAdmin,
   };
 }
--- src/modules/Workspace/components/Info/view.tsx
+++ src/modules/Workspace/components/Info/view.tsx
@@ -3,25 +3,31 @@ import { useAppSelector } from '@/redux/store';
 import Box from '@mui/material/Box';
 import Tabs from '@/components/Tab';
 import { selectWorkspaceDetails } from '@/redux/slices/workspace';
+import { useAuthorized } from '@/hooks';
 import WorkspaceHeader from './components/Header';
 import Sections from './components/Sections';
 import Insight from './components/Insight';
 
 function WorkspaceInfoView() {
   const workspace = useAppSelector(selectWorkspaceDetails);
+  const { isWorkspaceAdmin } = useAuthorized();
 
   return (
     <Box>
       <WorkspaceHeader workspaceName={workspace.name || ''} workspaceId={workspace._id || ''} />
-      <Tabs>
-        <Tabs.Header items={['Boards', 'Insight']} />
-        <Tabs.Panel index={0}>
-          <Sections />
-        </Tabs.Panel>
-        <Tabs.Panel index={1}>
-          <Insight />
-        </Tabs.Panel>
-      </Tabs>
+      {isWorkspaceAdmin ? (
+        <Tabs>
+          <Tabs.Header items={['Boards', 'Insight']} />
+          <Tabs.Panel index={0}>
+            <Sections />
+          </Tabs.Panel>
+          <Tabs.Panel index={1}>
+            <Insight />
+          </Tabs.Panel>
+        </Tabs>
+      ) : (
+        <Sections />
+      )}
     </Box>
   );
 }
