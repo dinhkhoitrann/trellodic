120d6bb39a37f5915e1ba42c4ceeab1b7e15960c
Tran Dinh Khoi <trandinhkhoi102@gmail.com>
2024-01-23 17:52:22 +0700

    #44: change box shadow for action button in AddToCard section

--- src/components/ActionButton/index.tsx
+++ src/components/ActionButton/index.tsx
@@ -10,7 +10,7 @@ const StyledButton = styled(Button)(({ theme }) => ({
   justifyContent: 'left',
   paddingLeft: '20px',
   minWidth: '200px',
-  boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14)',
+  boxShadow: '0px 2px 2px -2px rgba(0,0,0,0.2), 0px 4px 6px 0px rgba(0,0,0,0.14)',
 }));
 
 type ActionButtonProps = ButtonProps & { renderPopover: () => ReactNode };
